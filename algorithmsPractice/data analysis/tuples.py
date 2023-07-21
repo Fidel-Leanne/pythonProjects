7c6a6909c3d4514b9a6ccfa897b5e7c2d01517b1
fidelia <fidelialtm13@gmail.com>
2023-07-22 00:04:10 +0200

    python 4

---
+++ algorithmsPractice/data analysis/tuples.py
@@ -0,0 +1,14 @@
+tuple1= (7, 2,0, 9, 7, 6,0,6)
+
+print(tuple1)
+print(tuple1[1])
+print(len(tuple1))
+print(tuple1[-3])
+print(tuple1[2:])
+
+thistuple = ("john", "Mary", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+else:
+  print("subject not found")
