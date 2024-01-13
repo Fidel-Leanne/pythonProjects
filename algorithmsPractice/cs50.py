d6fe5f1848b3c37370789483ee53cbc3f2e53a0a
fidelia <fidelialtm13@gmail.com>
2024-01-13 17:02:00 +0200

    CS50 LOOPS

--- algorithmsPractice/cs50.py
+++ algorithmsPractice/cs50.py
@@ -1,45 +1,4 @@
-# def main():
-#     name = input('What is your name? ')
-#     age = input('What is your age? ')
-#     return name, age  # Return the values from the function
 
-# def hello():
-#     user_name, user_age = main()  # Call main to get name and age
-#     print(f'Name: {user_name}')
-#     print(f'Age: {user_age}')
-
-# # Call the hello function
-# hello()
-
-# x= int(input('Enter x'))
-# y= int(input('Enter y'))
-
-# if x==y:
-#     print('the entered values are equal')
-# else:
-#     print('the entered values are not equal')
-
-# score= int(input('Score'))
-
-# if 90<= score <=100:
-#     print("grade A")
-# elif 80<= score:
-#     print("grade B")
-# elif 70<= score:
-#     print("grade C")
-# elif 60<= score:
-#     print("grade D")
-# else:
-#     print("grade F")
-
-i = 0
-
-while i!= 3:
-    print('meow')
-    i+=1
-
-for _ in range(9):
-    print('bark')
 
 
 
