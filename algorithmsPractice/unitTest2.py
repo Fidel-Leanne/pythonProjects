ed845b24cb9d3f7a6c60a5d92d3c5005f03bdbb3
fidelia <fidelialtm13@gmail.com>
2024-01-16 21:37:42 +0200

    unit tests

---
+++ algorithmsPractice/unitTest2.py
@@ -0,0 +1,10 @@
+from unitTest import square
+
+def test_square():
+    
+        assert square(2)==4    
+        assert square(3)==9
+        assert square(7)==4
+        assert square(10)==50
+        
+
