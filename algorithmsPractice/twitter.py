a952b2a9954be0a36b08b70c047050ca7a758b5c
fidelia <fidelialtm13@gmail.com>
2024-01-18 23:29:56 +0200

    regex v3

--- algorithmsPractice/twitter.py
+++ algorithmsPractice/twitter.py
@@ -1,6 +1,8 @@
+import re
+
 url= input('URL').strip()
 
-username= url.replace('https://twitter.com/', '')
+if matches:= re.search(r'^https?://?(www\.)?twitter.com/', url, re.IGNORECASE):
 
-print(f'Username: {username}')
+    print(f'Username: ' , matches.group(1))
 
