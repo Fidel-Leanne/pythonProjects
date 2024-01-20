668b57f29b03ad0fabb14da561997d0ffe0ab59f
fidelia <fidelialtm13@gmail.com>
2024-01-20 21:11:30 +0200

    classes

--- algorithmsPractice/students.py
+++ algorithmsPractice/students.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self,name, house):
+    def __init__(self,name, house, patronus):
         if not name:
             raise ValueError('Missing name')
         if house not in [ 'Gryffindor', 'Hufflepuff', 'Ravenclaw','Slytherin']:
@@ -7,17 +7,36 @@ class Student:
         
         self.name = name
         self.house= house
+        self.patronus= patronus
+
+
+    def __str__(self):
+            return f'{self.name} from {self.house}'
+    
+    def charm(self):
+         match self.patronus:
+              case 'staging' :
+                   return '😀'
+              case 'otter':
+                   return '👍'
+              case 'Jack Russell terrier' :
+                   return '⭐'
+              case _:
+                   return '☔'
+         
 
 def main():
     student= get_student()
-    print(f'{student.name} from {student.house}')
+    print ('Expecto Patronous')
+    print(student.charm())
 
 
 def get_student():
    
     name=input('Name: ')
     house= input('House: ')
-    return Student(name , house)
+    patronus= input('Patronus: ')
+    return Student(name , house, patronus)
 
 
 if __name__ == '__main__':
