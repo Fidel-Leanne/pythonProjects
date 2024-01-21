73a81da37e5b100287b5c13ed0d17f294bdc2501
fidelia <fidelialtm13@gmail.com>
2024-01-21 22:13:35 +0200

    Properties and decorators

--- algorithmsPractice/students.py
+++ algorithmsPractice/students.py
@@ -1,42 +1,44 @@
 class Student:
-    def __init__(self,name, house, patronus):
-        if not name:
-            raise ValueError('Missing name')
-        if house not in [ 'Gryffindor', 'Hufflepuff', 'Ravenclaw','Slytherin']:
-            raise ValueError('invalid house')
+    def __init__(self,name, house):
         
         self.name = name
         self.house= house
-        self.patronus= patronus
-
+        
 
     def __str__(self):
             return f'{self.name} from {self.house}'
     
-    def charm(self):
-         match self.patronus:
-              case 'staging' :
-                   return '😀'
-              case 'otter':
-                   return '👍'
-              case 'Jack Russell terrier' :
-                   return '⭐'
-              case _:
-                   return '☔'
-         
+    @property
+    def name(self):
+         return self._name
+    
+    @name.setter
+    def name(self, name):
+         if not name:
+            raise ValueError('Missing name')
+         self._name = name
+    
+    @property    
+    def house(self):
+         return self._house
+    
+    @house.setter    
+    def house(self, house):
+         if house not in [ 'Gryffindor', 'Hufflepuff', 'Ravenclaw','Slytherin']:
+            raise ValueError('invalid house')
+         self._house = house
 
+    
 def main():
     student= get_student()
-    print ('Expecto Patronous')
-    print(student.charm())
+    print(student)
 
 
 def get_student():
    
     name=input('Name: ')
-    house= input('House: ')
-    patronus= input('Patronus: ')
-    return Student(name , house, patronus)
+    house= input('House: ') 
+    return Student(name , house)
 
 
 if __name__ == '__main__':
